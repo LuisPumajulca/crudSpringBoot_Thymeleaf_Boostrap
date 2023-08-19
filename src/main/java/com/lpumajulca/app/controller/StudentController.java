49877d454d588faa0b72d357f4eaecf566511e75
LuisPumajulca <pumajulca@hotmail.com>
2023-08-19 10:34:30 -0500

    agregando html para crear/registrar y  controllador

--- src/main/java/com/lpumajulca/app/controller/StudentController.java
+++ src/main/java/com/lpumajulca/app/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.lpumajulca.app.controller;
 
+import com.lpumajulca.app.entity.Student;
 import com.lpumajulca.app.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,5 +17,12 @@ public class StudentController {
     public String listStudents(Model model) {
         model.addAttribute("students", servicio.listAllStudents());
         return "students"; //nos retorna el archivo estudiante
+    }
+
+    @GetMapping("/students/new")
+    public String createStudentForm(Model model) {
+        Student student = new Student();
+        model.addAttribute("student", student);
+        return "create_student";
     }
 }
